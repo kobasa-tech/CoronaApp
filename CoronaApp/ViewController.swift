8ed4fdce1261da16b006e769cda232bc46c409ca
kobasa-tech <warped.trump@gmail.com>
2021-10-30 11:48:51 +0900

    グラデーションを使用した背景色の作成

--- CoronaApp/ViewController.swift
+++ CoronaApp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let colors = Colors()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpGradation()
+        
     }
 
-
+    // グラデーションの設定
+    func setUpGradation() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        // グラデーションの配色は配列で指定する
+        gradientLayer.colors = [colors.bluePurple.cgColor, colors.blue.cgColor]
+        // グラデーションの方向を設定している。0,0から始まって1,1方向の向き
+        gradientLayer.startPoint = CGPoint.init(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint.init(x: 1, y: 1)
+        /* レイヤーを載せる場合はinsertSublayer(子, at: 海藻)を使用する。
+         親であるスマホ画面に作成したグラデーションレイヤーを載せる。レイヤーを重ねるときはatの値が大きい方が前面にいく。*/
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
 
