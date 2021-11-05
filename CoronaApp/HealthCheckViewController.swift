ca022e250641fb23f9518ba0b89deb5b2037f9ab
kobasa-tech <warped.trump@gmail.com>
2021-11-05 19:58:11 +0900

    健康管理画面へ遷移

---
+++ CoronaApp/HealthCheckViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HealthCheckViewController.swift
+//  CoronaApp
+//
+//  Created by 小早川　聖 on 2021/11/05.
+//
+
+import UIKit
+
+class HealthCheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
